076f24fae214c6d84d2c496e62968e70ebe53fae
Darshan Nag <dndarshan2@gmail.com>
2024-11-20 15:40:53 +0000

    minor typos for congnito

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -22,7 +22,7 @@ Amplify.configure({
   "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
   "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
   "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
-  "aws_user_pools_web_client_id": process.env.REACT_APP_AWS_CLIENT_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
   "oauth": {},
   Auth: {
     // We are not using an Identity Pool
